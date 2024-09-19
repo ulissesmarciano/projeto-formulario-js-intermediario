ad095db08ce5c3cdfb10bb82432fba2f597495aa
Ulisses <ulissesmkt2104@gmail.com>
2024-09-19 15:06:32 -0300

    Adicionando método para controle do form

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -1,10 +1,12 @@
 const todosOsInputs = document.querySelectorAll(".input")
 const botaoEnviar = document.querySelector(".botao")
 const todasAsAreas = document.querySelectorAll(".area-do-input")
+const formulario = document.querySelector(".formulario")
 
 
 
-botaoEnviar.addEventListener('click', function () {
+botaoEnviar.addEventListener('click', function (event) {
+    event.preventDefault()//busquei no stackoverflow a resposta de por que o form faz um submit e vi que a resposta era utilizar este método. Somente assim encontrei uma solução para a tag form. Se não teria que utilizar uma div.
     for (let i = 0; i < todosOsInputs.length; i++) {
         const input = todosOsInputs[i].value
         const areaDoInput = todasAsAreas[i]
