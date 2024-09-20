e33da699e564aaffb2863f57f4ed25b6baa0895c
Ulisses <ulissesmkt2104@gmail.com>
2024-09-20 14:49:26 -0300

    Criando lógica para o change dos inputs.

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -4,22 +4,29 @@ const todasAsAreas = document.querySelectorAll(".area-do-input")
 const formulario = document.querySelector(".formulario")
 
 
-
-botaoEnviar.addEventListener('click', function (event) {
-    event.preventDefault()//busquei no stackoverflow a resposta de por que o form faz um submit e vi que a resposta era utilizar este método. Somente assim encontrei uma solução para a tag form. Se não teria que utilizar uma div.
-    for (let i = 0; i < todosOsInputs.length; i++) {
-        const input = todosOsInputs[i].value
-        const areaDoInput = todasAsAreas[i]
-
-        if (input !== "") {
-            areaDoInput.classList.add('validado')
-            areaDoInput.classList.remove('invalidado') 
-        } else {
-            areaDoInput.classList.remove('validado')
-            areaDoInput.classList.add('invalidado')
-        }
-        
+function validarInput (input, areaDoInput) {
+    if (input.value !== "") {
+        areaDoInput.classList.remove('invalidado')
+        areaDoInput.classList.add('validado')
+    }   else {
+        areaDoInput.classList.add('invalidado')
+        areaDoInput.classList.remove('validado')
     }
+}
+
+
+botaoEnviar.addEventListener('click', function (evento) {
+    evento.preventDefault()//busquei no stackoverflow a resposta de por que o form faz um submit e vi que a resposta era utilizar este método. Somente assim encontrei uma solução para a tag form. Se não teria que utilizar uma div.
+    for (let i = 0; i < todosOsInputs.length; i++) {
+        validarInput(todosOsInputs[i], todasAsAreas[i])
+    }
+})
+
+todosOsInputs.forEach((input, index) => {
+    const areaDoInput = todasAsAreas[index]
+    input.addEventListener('change', () => {
+        validarInput(input, areaDoInput)
+    })
 })
 
 
